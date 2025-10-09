100b7bf8adafe6807863a329a25a5503a679ba86
saarock <saarock4646@gmail.com>
2025-10-09 14:08:59 +0545

    changes the content

--- src/sections/Hub/Hub.tsx
+++ src/sections/Hub/Hub.tsx
@@ -7,8 +7,7 @@ const Hub = () => {
           <span className="text-orange-500">Innovation</span>
         </h2>
         <p className="text-gray-600 text-base sm:text-lg lg:text-xl leading-relaxed max-w-3xl mx-auto">
-          SUMS empowers to create opportunity to every stakeholder through
-          innovative connections and real-world solutions.
+         Empowering learners, academia, and industry with real-world innovation.
         </p>
       </div>
     </section>
